0a5f0dad4ec466d892a9244874179c555ab6bffd
iamvetle <vetle122@gmail.com>
2023-08-08 22:02:46 +0200

    .

--- newproject/src/App.vue
+++ newproject/src/App.vue
@@ -3,35 +3,78 @@
 <!--  border-2 border-dashed border-red-800 -->
 
 
-<div class="pt-9 pb-9 h-auto" > <!-- CONTAINER FOR HEADER/NAVBAR-->
+<div class="pt-9 pb-9" > <!-- FLEX-CONTAINER FOR HEADER/NAVBAR-->
   <header id="header" class="mx-auto w-11/12 lg:w-4/6 xl:w-[62.5%]">
       <img class="ms-2 inline" src="vite.svg" alt="Logo"> <!-- 2/12-->
-      <ul class=" list-none inline float-right space-x-8"> <!-- 10/12 -->
-        <li class="me-3 inline font-semibold">Hello</li>
-        <li class="me-3 inline font-semibold">Product</li>
-        <li class="me-3 inline font-semibold">About</li>
-        <li class="me-3 inline font-semibold">Searhc?</li>
-        <li class="me-3 inline font-semibold">Contact</li>
-        <li class="rounded-sm inline bg-blue-800 py-2 px-3 text-white">CALL TO ACTION</li>
-      </ul>
+        <ul id="menu-links" class=" list-none inline float-right space-x-8"> <!-- 10/12 -->
+          <li class="me-3 inline font-semibold">Hello</li>
+          <li class="me-3 inline font-semibold">Product</li>
+          <li class="me-3 inline font-semibold">About</li>
+          <li class="me-3 inline font-semibold">Searhc?</li>
+          <li class="me-3 inline font-semibold">Contact</li>
+          <li class="rounded-sm inline bg-blue-800 py-2 px-3 text-white">CALL TO ACTION</li>
+        </ul>
   </header>
 </div>
 
   <main> <!-- h1 and p, 8/12-->
-    <div class="bg-gray-600">
+    <div class="bg-gray-600"> <!-- FLEX-CONTAINER for Hero section -->
       
-      <section class=" hero-section h-[70vh] text-white bg-gray-600 w-11/12 lg:w-4/6 xl:w-[62.5%] mx-auto">
+      <section class="hero-section min-h-[550px] h-[70vh] text-white bg-gray-600 w-11/12 lg:w-4/6 xl:w-[62.5%] mx-auto">
         
-        <div class="w-8/12"> <!-- 8/12 -->
-          <h1 class="text-7xl ms-[10%] mt-[15%] font-medium">The Headline that saves Lives</h1>
-          <p class="ms-[10%] mt-7 w-3/5 text-lg leading-8">I can do it. Therefore you can take your trust in my hands. Then you can do it.</p> <!-- Description 1-3 lines-->
-          <button class="ms-[10%] mt-9 bg-blue-800 text-white px-5 py-4 rounded-md">SIGN UP NOW</button>
+        <div class="w-8/12 ps-[10%] pt-[15%] "> <!-- 8/12 -->
+          <h1 class="text-7xl font-medium">The Headline that saves Lives</h1>
+          <p class="pt-7 w-3/5 text-lg leading-8">I can do it. Therefore you can take your trust in my hands. Then you can do it.</p> <!-- Description 1-3 lines-->
+          <button class="mt-9 bg-blue-800 text-white px-5 py-4 rounded-md">SIGN UP NOW</button>
         </div>
 
           <div class="4/12"></div> <!-- 4/12 -->
       </section>
-    </div> 
-  
+    </div>
+
+      <!-- ROW 1 -->
+      <div class="bg-[#F7F7F7]"> <!-- FLEX-CONTAINER for row-->
+        <section id="about-me" class="pt-5 mx-auto w-11/12 lg:w-4/6 xl:w-[62.5%]">
+          <div class="inline w-6/12 float-left">          
+            <img :src="aboutus" class="rounded-sm" alt="About us">
+          </div>
+          <div class="inline w-6/12 text-lg float-right">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque perferendis porro ex recusandae saepe ea aperiam consectetur mollitia eaque. Facilis repellat iste id necessitatibus vero temporibus blanditiis perspiciatis cumque quisquam!
+          </div>
+        </section>
+      </div>
+
+      <hr>
+
+      <div class="bg-[#F7F7F7] clear-both"> <!-- FLEX-CONTAINER for row-->
+        <section id="about-me" class="pt-5 mx-auto w-11/12 lg:w-4/6 xl:w-[62.5%]">
+          <div class="w-6/12">          
+            <img :src="aboutus" class="rounded-sm" alt="About us">
+          </div>
+          <div class="w-6/12 text-lg">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque perferendis porro ex recusandae saepe ea aperiam consectetur mollitia eaque. Facilis repellat iste id necessitatibus vero temporibus blanditiis perspiciatis cumque quisquam!
+          </div>
+        </section>
+      </div>
+
+
+
+      <!-- Color row: #FFFFFF -->
+      <!-- Color row: #353535 -->
+
+
+      <!-- Color row: #FFFFFF -->
+      <!-- Color row: #F7F7F7 -->
+      <!-- Color row: #FFFFFF -->
+      <!-- Color row: #F8F2EF -->
+      <!-- Color row: #FFFFFF -->
+      <!-- Color row: #F8F8F8 -->
+      <!-- Color row: #FFFFFF -->
+
+      <!-- Color row: #353535 -->
+
+
+
   </main>
   <hr id="fold">
 
@@ -40,10 +83,13 @@
 
 <script>
 
+import aboutus from './assets/people-2557396_960_720.jpg'
+
 export default {
   data() {
     return {
-      logo: '/vites'
+      logo: '/vites',
+      aboutus
 
     }
   }
